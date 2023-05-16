2e2d7f4d832dd1d4aa4f3fe0fc0b5ee0b98a169c
Gutis007 <101084525+Gutis-007@users.noreply.github.com>
2023-05-16 20:50:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const form = document.getElementById('form');
+form.addEventListener('submit',handleSubmit)
+const inputValue = document.getElementById('value-real');
+const selectedCurrency = document.getElementById('currency');
+const result = document.getElementById('result');
+
+function handleSubmit(e){
+    
+    e.preventDefault();
+    if(!inputValue.value || inputValue.value<0){
+        alert("Informe um valor correto!");
+        return;
+    }
+    else if(!selectedCurrency.value){
+        alert("Selecione uma moeda!");
+        return;
+    }
+    converter();
+}
+
+function converter(){
+    if(selectedCurrency.value === 'eur'){
+        valueConverted = inputValue.value/ 5.37;
+        result.innerHTML  = valueFormatter('pt-BR','EUR');
+
+        animateResult()
+    }
+    else if (selectedCurrency.value === 'dol'){
+        valueConverted = inputValue.value/ 4.94;
+        result.innerHTML = valueFormatter('en-US','USD');
+
+        animateResult()
+    }
+    
+    inputValue.value = '';
+    selectedCurrency.value = '';
+}
+
+function valueFormatter(locale,currency) {
+    const value = valueConverted.toLocaleString(`${locale}`,{style: 'currency',currency: `${currency}`})
+    return `<span>🤑</span> ${value}<span>🤑</span>`
+}
+
+function animateResult(){
+    return result.animate([
+        {transform: 'translateY(-130px)'},
+        {transform: 'translateX(0px)'},
+    ],{duration:500})
+}
